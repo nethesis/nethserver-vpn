85cfc7b2cb3756f45d885c02b36ebfa53fd48624
Victor Moreno <victor_moreno_rubio@hotmail.com>
2014-05-05 12:57:29 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_VPN_Accounts.php
@@ -0,0 +1,25 @@
+<?php 
+
+/* NethServer_Module_VPN_VPNAccounts translation, language: es */
+
+$L['bridged_label'] = 'Puente';
+$L['create_header_label'] = 'Crear cuenta nueva';
+$L['Key_label'] = 'Nombre';
+$L['name_label'] = 'Nombre';
+$L['VPNRemoteNetmask_label'] = 'Máscara de red';
+$L['VPNRemoteNetwork_label'] = 'Dirección de red';
+$L['RemoteNetwork_label'] = 'Red remota';
+$L['routed_label'] = 'Enrutado';
+$L['update_header_label'] = 'Modificar cuenta';
+$L['Accounts_Title'] = 'Cuentas';
+$L['AccountType_label'] = 'Tipo de cuenta';
+$L['AccountType_user_label'] = 'Usuario del sistema';
+$L['AccountType_vpn_label'] = 'VPN-only';
+$L['Expiration_label'] = 'Certificado de caducidad';
+$L['User_label'] = 'Usuario';
+$L['download_label'] = 'Descargar';
+$L['download_ovpn'] = 'Configuración de OpenVPN';
+$L['download_pem'] = 'Certificado, la clave privada y CA (Formato PEM)';
+$L['download_ca'] = 'Certificado de CA x509 (Formato PEM)';
+$L['download_pkcs12'] = 'Certificado y clave privada (Formato PKCS12)';
+$L['download-vpn_Header'] = 'Descargar';
---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_VPN_Clients.php
@@ -0,0 +1,27 @@
+<?php 
+
+/* NethServer_Module_VPN_VPNClients translation, language: es */
+
+$L['AuthMode_certificate_label'] = 'Certificado';
+$L['AuthMode_label'] = 'Autentificación';
+$L['AuthMode_password-certificate_label'] = 'El usuario, contraseña y certificado';
+$L['AuthMode_password_label'] = 'El usuario y la contraseña';
+$L['AuthMode_psk_label'] = 'Clave precompartida';
+$L['Mode_bridged_label'] = 'Puente';
+$L['create_header_label'] = 'Crear nuevo cliente';
+$L['Crt_label'] = 'Contenido de certificados';
+$L['Key_label'] = 'Nombre';
+$L['Mode_label'] = 'Modo';
+$L['name_label'] = 'Nombre';
+$L['Password_label'] = 'Contraseña';
+$L['Psk_label'] = 'Clave';
+$L['RemoteHost_label'] = 'El host remoto';
+$L['RemotePort_label'] = 'Puerto remoto';
+$L['Mode_routed_label'] = 'Enrutado';
+$L['update_header_label'] = 'Modificar cliente';
+$L['User_label'] = 'Usuario';
+$L['Clients_Title'] = 'Cliente';
+$L['VPNType_ipsec_label'] = 'IPsec';
+$L['VPNType_label'] = 'Tipo';
+$L['VPNType_openvpn_label'] = 'OpenVPN';
+$L['Compression_label'] = 'Activar compresión LZO';
